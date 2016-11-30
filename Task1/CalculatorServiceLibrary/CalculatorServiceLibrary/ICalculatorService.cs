f032dfd3507ad0208a0637f71ef6370e0b373883
Дмитрий Чернов <dm.chernov@outlook.com>
2016-11-30 20:39:24 +0400

    new operations was added

---
+++ Task1/CalculatorServiceLibrary/CalculatorServiceLibrary/ICalculatorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorServiceLibrary
+{
+    [ServiceContract]
+    public interface ICalculatorService
+    {
+        [OperationContract]
+        [WebGet]
+        int Add(int a, int b);
+        [OperationContract]
+        [WebGet]
+        int Mul(int a, int b);
+		[OperationContract]
+        [WebGet]
+        int Sub(int a, int b);
+		[OperationContract]
+        [WebGet]
+        int Div(int a, int b);
+    }
+}
---
+++ Task1/CalculatorServiceLibrary/ConsoleHost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			var host = new ServiceHost(typeof(CalculatorServiceLibrary.CalculatorService));
+
+            host.Open();
+            Console.WriteLine("Service started");
+
+            Console.ReadLine();
+
+            host.Close();
+            Console.WriteLine("Service closed");
+        }
+    }
+}

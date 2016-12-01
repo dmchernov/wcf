cef35d5af2dfe68f34ed7d439d5af928b70d053f
Дмитрий Чернов <dm.chernov@outlook.com>
2016-12-01 19:00:44 +0400

    Task 1 complete

---
+++ Task1/CalculatorServiceLibrary/TestClient/TcpServiceTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestClient
+{
+	[TestClass]
+	public class TcpServiceTest
+	{
+		[TestMethod]
+		public void AddTest ()
+		{
+			using (var client = new TcpServiceReference.CalculatorServiceClient())
+			{
+				Console.WriteLine(client.Add(5, 71));
+				Assert.AreEqual(client.Add(5, 5), 10);
+			}
+		}
+
+		[TestMethod]
+		public void MulTest ()
+		{
+			using (var client = new TcpServiceReference.CalculatorServiceClient())
+			{
+				Console.WriteLine(client.Mul(5, 5));
+				Assert.AreEqual(client.Mul(5, 5), 25);
+			}
+		}
+
+		[TestMethod]
+		public void SubTest ()
+		{
+			using (var client = new TcpServiceReference.CalculatorServiceClient())
+			{
+				Console.WriteLine(client.Sub(10, 5));
+				Assert.AreEqual(client.Sub(10, 1), 9);
+			}
+		}
+
+		[TestMethod]
+		public void DivTest ()
+		{
+			using (var client = new TcpServiceReference.CalculatorServiceClient())
+			{
+				Console.WriteLine(client.Div(10, 5));
+				Assert.AreEqual(client.Div(10, 5), 2);
+			}
+		}
+	}
+}
